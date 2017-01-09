2ad5807e26cb0d00bf4589cc1feaa35aa9e93ea6
Bob Liu <bob.liu@nbcuni.com>
2017-01-09 10:09:15 -0500

    adding drush alias

---
+++ local_aliases.drushrc.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Comcast RSN Drush site aliases
+ */
+
+/*
+//Local, primary developement environment
+$aliases['localhost'] = array(
+	'uri' => 'localhost',
+	'root' => '/Users/nbcmacminiqa/Desktop/sites/GolfChannel-BitBucket/docroot',
+	'path-aliases' => array(
+		'%files' => 'sites/localhost/files',
+		'%dump-dir' => '/tmp/',
+		'%drush' => '/usr/bin/drush',
+		),
+	);
+
+*/
+
+//Local, primary developement environment
+$aliases['localhost'] = array(
+	'uri' => 'localhost',
+	'root' => '/Users/nbcmacminiqa/Desktop/sites/telemundo-deportes/docroot/',
+	'path-aliases' => array(
+		'%files' => 'sites/default/files',
+		'%dump-dir' => '/tmp/',
+		'%drush' => '/usr/bin/drush',
+		),
+	);
+
